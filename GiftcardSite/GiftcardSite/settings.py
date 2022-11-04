038129a6fba6eeda08c23fd2c55ad59eec1d6458
Samuel Lopez <lopez.samuel.4196@gmail.com>
2022-11-04 00:22:20 -0400

    test3

--- GiftcardSite/GiftcardSite/settings.py
+++ GiftcardSite/GiftcardSite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DjangoSecret') #Key stored in OS enviroment variable DjangoSecret
+SECRET_KEY = os.environ.get('DjangoSecret') #Key stored in OS enviroment variable DjangoSecret.
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
