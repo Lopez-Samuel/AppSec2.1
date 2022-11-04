685623e4be7ef2dfd76588faff28ec36ab5f72c6
Samuel Lopez <lopez.samuel.4196@gmail.com>
2022-11-04 00:23:36 -0400

    Delete csrfmain.py

--- GiftcardSite/csrfmain.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-s = requests.session()
-cookie = s.cookies.get_dict()
-get_token = s.get("http://127.0.0.1/gift.html")
-if ('<input type="hidden" name="csrfmiddlewaretoken' in (get_token.text)):
-	print("Not vulnerable to CSRF!")
-else:
-	print ("Vulnerable to CSRF!")
